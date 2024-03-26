b6c9b96489c79a3e042f0d24b67dab64d118acaa
WENG JIA HONG <153618357+ahsbdcpu@users.noreply.github.com>
2024-03-26 21:52:17 +0800

    Update application.py

--- application/application.py
+++ application/application.py
@@ -5,7 +5,6 @@ from flask_caching import Cache
 from config import TEMPLATES_PATH, TEXT_PATH
 from application.helpers import *
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__, template_folder=TEMPLATES_PATH)
 app.jinja_env.filters["is_active"] = is_active
@@ -84,16 +83,3 @@ def result():
     """Renders the 'Result' page of the website."""
 
     return render_template("result.html")
-
-@app.route('/generate', methods=['POST'])
-def generate():
-    prompt = request.form['prompt']
-    response = openai.ChatCompletion.create(
-        messages=[
-            {"role": "user", "content": prompt}
-        ],
-        model="gpt-3.5-turbo-0125",
-        temperature = 0.5,
-    )
-    generated_text = response['choices'][0]['message']['content'].strip()
-    return render_template('index1.html', response=generated_text)
